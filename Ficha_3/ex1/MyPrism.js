e3ab845c4443e0ebf1efdc913508777a60858e67
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-03-11 20:32:24 +0000

    Renamed class :facepalm:

--- Ficha_3/ex1/MyPrism.js
+++ Ficha_3/ex1/MyPrism.js
@@ -4,7 +4,7 @@
  * @constructor
  */
 
-class MyQuad extends CGFobject
+class MyPrism extends CGFobject
 {
 	constructor(scene, slices, stacks) 
 	{
@@ -26,27 +26,30 @@ class MyQuad extends CGFobject
 
 		// fill vertices and normals array
 		/**
-		 * Seja Θ = 2*PI / numero_lados
-		 * Para i = 0 até i = numero_lados-1
-		 * 		Adiciona vértice (cos(i*Θ), sin(i*Θ), 0) duas vezes (devido às normais)
-		 * 		Adiciona duas normais
-		 * 			- (cos(Θ+(i+0.5)), sin(Θ+(i+0.5)), 0)
-		 * 			- (cos(Θ+(i-0.5)), sin(Θ+(i-0.5)), 0)
-		 * 		i <- i + 1
+		 * Let Θ = 2*PI / slices (angulo externo)
+		 * 
+		 * For i = 0 till i = stacks - 1
+		 * 		
+		 * 		For j = 0 till j = slices - 1
+		 * 			Add two vertices, because each will have a different normal [cos(j*Θ), sin(j*Θ), i]
+		 * 			Add the two normals to each vertice
+		 * 				- (cos(Θ*(j+0.5)), sin(Θ*(j+0.5)), i)
+		 * 				- (cos(Θ*(j-0.5)), sin(Θ*(j-0.5)), i)
+		 * 			j <- j + 1
 		 */
 
-		 /**
+		/**
 		  * Fill indices array
 		  * Vertices are counter-clock sorted
-		  * Para i = 0 até i = vertices.length
-		  * 	Adiciona [i, i+2, i+4]
-		  * 	Adiciona [i+1, i+3, i+5]
-		  * 	i += numero_lados*2
+		  * For i = 0 till i = vertices.length
+		  * 	add [i, i+2, i+4]
+		  * 	add [i+1, i+3, i+5]
+		  * 	i += slices*2
 		  */
 
 		// Array methods like push might be useful here
 		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Indexed_collections
-			
+					
 		this.primitiveType=this.scene.gl.TRIANGLES;
 				 
 		this.initGLBuffers();
