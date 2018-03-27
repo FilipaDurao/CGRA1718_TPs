df01ba6b142e917b7524bacc04156b1fc351fb93
Fabio Gasspar <fabiodanielreis@hotmail.com>
2018-03-27 17:16:17 +0100

    Changed LightingScene to be the same as  Ficha_2/extra

--- Ficha_4/ex1/LightingScene.js
+++ Ficha_4/ex1/LightingScene.js
@@ -29,14 +29,42 @@ class LightingScene extends CGFscene
 
 		this.axis = new CGFaxis(this);
 
+		// Scene elements
+		this.table = new MyTable(this);
+		this.wall = new Plane(this);
+		this.floor = new MyQuad(this);
+		this.chair = new MyChair(this);
 		
-		// Prism
-		this.prismA = new MyPrism(this, 8, 20);
-		this.cilA = new MyCylinder(this, 8, 20);
+		this.boardA = new Plane(this, BOARD_A_DIVISIONS);
+		this.boardB = new Plane(this, BOARD_B_DIVISIONS);
 
 		// Materials
 		this.materialDefault = new CGFappearance(this);
 		
+		this.materialA = new CGFappearance(this);
+		this.materialA.setAmbient(0.3,0.3,0.3,1);
+		this.materialA.setDiffuse(0.6,0.6,0.6,1);
+		this.materialA.setSpecular(0,0.2,0.8,1);
+		this.materialA.setShininess(120);
+
+		this.materialB = new CGFappearance(this);
+		this.materialB.setAmbient(0.3,0.3,0.3,1);
+		this.materialB.setDiffuse(0.6,0.6,0.6,1);
+		this.materialB.setSpecular(0.8,0.8,0.8,1);	
+		this.materialB.setShininess(120);
+
+		this.materialC = new CGFappearance(this);
+		this.materialC.setAmbient(0.3,0.3,0.3,1);
+		this.materialC.setDiffuse(0,0,1,1);
+		this.materialC.setSpecular(0,0,1,1);	
+		this.materialC.setShininess(120);
+
+		this.materialD = new CGFappearance(this);
+		this.materialD.setAmbient(0.3,0.3,0.3,1);
+		this.materialD.setDiffuse(1,0,0,1);
+		this.materialD.setSpecular(1,0,0,1);	
+		this.materialD.setShininess(120);
+
 	};
 
 	initCameras() 
@@ -46,17 +74,55 @@ class LightingScene extends CGFscene
 
 	initLights() 
 	{
-		this.setGlobalAmbientLight(0.3,0.3,0.3, 1.0);
+		//this.setGlobalAmbientLight(0.5,0.5,0.5, 1.0);
+		
+		// Positions for four lights
 
-
+		// Create light 0
+		this.lights[0].setPosition(4, 6, 1, 1);
+		this.lights[0].setVisible(true); // show marker on light position (different from enabled)
+		
 		// Create light 1
-		this.lights[0].setPosition(2.5, 4, 0, 1.0);
-		this.lights[0].setVisible(true); // show marker on light position (different from enabled)	
-		// Light 1 caractheristics
+		this.lights[1].setPosition(10.5, 6.0, 1.0, 1.0);
+		this.lights[1].setVisible(true); // show marker on light position (different from enabled)
+
+		// Create light 2 (Ex 3 Ponto 1)
+		this.lights[2].setPosition(10.5, 6.0, 5.0, 1.0);
+		this.lights[2].setVisible(true); // show marker on light position (different from enabled)
+
+		// Create light 3 (Ex 3 Ponto 4)
+		this.lights[3].setPosition(4, 6.0, 5.0, 1.0);
+		this.lights[3].setVisible(true); // show marker on light position (different from enabled)
+		
+		// Light 0 caractheristics
 		this.lights[0].setAmbient(0, 0, 0, 1);
 		this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[0].setSpecular(255, 255, 255, 1.0);
 		this.lights[0].enable();
+		
+		// Light 1 caractheristics
+		this.lights[1].setAmbient(0, 0, 0, 1);
+		this.lights[1].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[1].setSpecular(255, 255, 255, 1.0);
+		this.lights[1].enable();
 
+		// Light 2 caractheristics (Ex 3 enable light 2 and set atenuation factors)
+		this.lights[2].setAmbient(0, 0, 0, 1);
+		this.lights[2].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[2].setSpecular(255, 255, 255, 1);
+		this.lights[2].setConstantAttenuation(0);
+		this.lights[2].setLinearAttenuation(1.0);
+		this.lights[2].setQuadraticAttenuation(0);
+		this.lights[2].enable();
+
+		// Light 3 caractheristics (Ex 3 enable light  3 and set attenuation factors)
+		this.lights[3].setAmbient(0, 0, 0, 1);
+		this.lights[3].setDiffuse(1.0, 1.0, 1.0, 1.0);
+		this.lights[3].setSpecular(255, 255, 255, 1.0);
+		this.lights[3].setConstantAttenuation(0);
+		this.lights[3].setLinearAttenuation(0);
+		this.lights[3].setQuadraticAttenuation(1.0);
+		this.lights[3].enable();
 
 	};
 
@@ -94,20 +160,75 @@ class LightingScene extends CGFscene
 
 		// ---- BEGIN Scene drawing section
 
-
+		// Floor
 		this.pushMatrix();
-			this.scale(1, 1, 6);
-			this.prismA.display();
+			this.translate(7.5, 0, 7.5);
+			this.rotate(-90 * degToRad, 1, 0, 0);
+			this.scale(15, 15, 0.2);
+			this.materialD.apply();
+			this.floor.display();
 		this.popMatrix();
 
-
+		// Left Wall
 		this.pushMatrix();
-			this.scale(1, 1, 6);
-			this.translate(5, 0, 0);
-			this.cilA.display();
+			this.translate(0, 4, 7.5);
+			this.rotate(90 * degToRad, 0, 1, 0);
+			this.scale(15, 8, 0.2);
+			this.materialC.apply();
+			this.wall.display();
 		this.popMatrix();
+
+		// Plane Wall
+		this.pushMatrix();
+			this.translate(7.5, 4, 0);
+			this.scale(15, 8, 0.2);
+			this.wall.display();
+		this.popMatrix();
+
+		// First Table
+		this.pushMatrix();
+			this.translate(5, 0, 8);
+			this.table.display();
+		this.popMatrix();
+
+		// Second Table
+		this.pushMatrix();
+			this.translate(12, 0, 8);
+			this.table.display();
+		this.popMatrix();
+
+		// Board A
+		this.pushMatrix();
+			this.translate(4, 4.5, 0.2);
+			this.scale(BOARD_WIDTH, BOARD_HEIGHT, 1);
 			
+			this.materialA.apply();
+			this.boardA.display();
+		this.popMatrix();
+
+		// Board B
+		this.pushMatrix();
+			this.translate(10.5, 4.5, 0.2);
+			this.scale(BOARD_WIDTH, BOARD_HEIGHT, 1);
+			
+			this.materialB.apply();
+			this.boardB.display();
+		this.popMatrix();
+
+		// First Chair
+		this.pushMatrix();
+			this.translate(5, 0, 10);
+			this.rotate(Math.PI, 0, 1, 0);
+			this.chair.display();
+		this.popMatrix();
+
+		// Second Chair
+		this.pushMatrix();
+			this.translate(12, 0, 10);
+			this.rotate(Math.PI, 0, 1, 0);
+			this.chair.display();
+		this.popMatrix();
+
 		// ---- END Scene drawing section
-		
 	};
 };
