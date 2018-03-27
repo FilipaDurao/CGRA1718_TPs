cace4893cb8fb5f68f5c5bbd726d862a43edbeaa
Fabio Gasspar <fabiodanielreis@hotmail.com>
2018-03-27 17:16:46 +0100

    Set texture coordinates

--- Ficha_4/ex1/MyQuad.js
+++ Ficha_4/ex1/MyQuad.js
@@ -4,37 +4,40 @@
  * @constructor
  */
 
-class MyQuad extends CGFobject
-{
-	constructor(scene) 
-	{
+class MyQuad extends CGFobject {
+	constructor(scene) {
 		super(scene);
 		this.initBuffers();
 	};
 
-	initBuffers() 
-	{
+	initBuffers() {
 		this.vertices = [
-				0.5, 0.5, 0,
-				0.5, -0.5, 0,
-				-0.5, 0.5, 0,
-				-0.5, -0.5, 0
+			0.5, 0.5, 0,
+			0.5, -0.5, 0, 
+			-0.5, 0.5, 0, 
+			-0.5, -0.5, 0
 		];
 
 		this.indices = [
-				0, 2, 1,
-				1, 2, 3
+			0, 2, 1,
+			1, 2, 3
 		];
-			
-		this.primitiveType=this.scene.gl.TRIANGLES;
-		
+
 		this.normals = [
-				0, 0, 1,
-				0, 0, 1,
-				0, 0, 1,
-				0, 0, 1
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1,
+			0, 0, 1
 		];
-				 
+
+		this.texCoords = [
+			0, 0,
+			0, 1,
+			1, 0,
+			1, 1
+		]
+
+		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
 	};
 };
