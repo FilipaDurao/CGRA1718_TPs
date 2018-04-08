b5dee36092a296618fdbca985d7e3c2230a6f4ae
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-08 12:07:25 +0100

    Modified MyQuad constructor

--- Ficha_4/ex1/MyQuad.js
+++ Ficha_4/ex1/MyQuad.js
@@ -5,8 +5,12 @@
  */
 
 class MyQuad extends CGFobject {
-	constructor(scene) {
+	constructor(scene, minS, maxS, minT, maxT) {
 		super(scene);
+		this.minS = minS;
+		this.maxS = maxS;
+		this.minT = minT;
+		this.maxT = maxT;
 		this.initBuffers();
 	};
 
@@ -31,11 +35,11 @@ class MyQuad extends CGFobject {
 		];
 
 		this.texCoords = [
-			0, 0,
-			0, 1,
-			1, 0,
-			1, 1
-		]
+			this.minS, this.minT,
+			this.minS, this.maxT,
+			this.maxS, this.minT,
+			this.maxS, this.maxT
+		];
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
 		this.initGLBuffers();
