1371f325212d4aff5aa9262196aff01cd6fe1f8b
filipa <filipadurao@gmail.com>
2018-04-23 18:02:43 +0100

    Fixed clock hands' movement

--- Ficha_5/ex1/MyClock.js
+++ Ficha_5/ex1/MyClock.js
@@ -21,6 +21,10 @@ class MyClock extends CGFobject
         this.minutesPointer = new MyClockHand(this.scene);
         this.secondsPointer = new MyClockHand(this.scene);
 
+        this.currHoursAngle = 90; 
+        this.currMinutesAngle = 180; 
+        this.currSecondsAngle = 270; 
+
         // define the texture to apply over the cylinder top
         this.cylinderTopAppearance = new CGFappearance(this.scene);
 		this.cylinderTopAppearance.loadTexture("../resources/images/clock.png");
@@ -41,7 +45,11 @@ class MyClock extends CGFobject
         this.secondsPointerAppearance.setAmbient(1, 0, 0, 1);
 	};
 
-	update(deltaTime){}
+	update(deltaTime){
+		this.currHoursPointer += ((6/60)/60)/10;
+		this.currMinutesAngle += (6/60)/10;
+		this.currSecondsAngle += 6/10;
+	}
 
 	display() {
         this.scene.pushMatrix();
@@ -52,7 +60,7 @@ class MyClock extends CGFobject
         this.scene.pushMatrix();
             this.hoursPointerAppearance.apply();
             this.scene.translate(0,0,1);
-            this.hoursPointer.setAngle(90);
+            this.hoursPointer.setAngle(this.currHoursAngle);
             this.scene.scale(0.021,0.5,0.02);	// Grossura x, Comprimento,  Grossura z 
             this.hoursPointer.display();
         this.scene.popMatrix();
@@ -61,7 +69,7 @@ class MyClock extends CGFobject
         this.scene.pushMatrix();
             this.minutesPointerAppearance.apply();
             this.scene.translate(0,0,1);
-            this.minutesPointer.setAngle(180);
+            this.minutesPointer.setAngle(this.currMinutesAngle);
             this.scene.scale(0.015, 0.9, 0.015);
             this.minutesPointer.display();
         this.scene.popMatrix();
@@ -70,7 +78,7 @@ class MyClock extends CGFobject
         this.scene.pushMatrix();
             this.secondsPointerAppearance.apply();
             this.scene.translate(0,0,1);            
-            this.secondsPointer.setAngle(270);
+            this.secondsPointer.setAngle(this.currSecondsAngle);
             this.scene.scale(0.004, 0.9, 0.004);
             this.secondsPointer.display();
         this.scene.popMatrix();
